45a31cda83e3dc8008c1ffaad458a9d48477d320
Yury Tarabanko <tarabanko@mirapolis.ru>
2019-05-27 16:20:14 +0300

    Updates

---
+++ precache-manifest.ee9e68a4061da4d11dd13c944dcd4fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1f73e2a37cb169bc62453232790379",
+    "url": "/incident-report-viewer/index.html"
+  },
+  {
+    "revision": "a7b0869ebf2165650a89",
+    "url": "/incident-report-viewer/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "eddb017f4670d684948a",
+    "url": "/incident-report-viewer/static/css/main.72af03ef.chunk.css"
+  },
+  {
+    "revision": "a7b0869ebf2165650a89",
+    "url": "/incident-report-viewer/static/js/2.25bfd736.chunk.js"
+  },
+  {
+    "revision": "eddb017f4670d684948a",
+    "url": "/incident-report-viewer/static/js/main.8659474f.chunk.js"
+  },
+  {
+    "revision": "7bd329e9ad6cccf52ddf",
+    "url": "/incident-report-viewer/static/js/runtime~main.d497f0d4.js"
+  }
+]);
